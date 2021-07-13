affb777a2a217f89b1c443ee7504844da12c4151
sedin <sedin@kakao.com>
2021-07-13 19:47:14 +0900

    Feat: BoardApiController crud 메소드 정의
    - BoardApiController에 구현할 메소드 뼈대 작성

--- app/src/main/java/board/controller/api/BoardApiController.java
+++ app/src/main/java/board/controller/api/BoardApiController.java
@@ -1,8 +1,43 @@
 package board.controller.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/boards")
 public class BoardApiController {
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create() {
+
+    }
+
+    @GetMapping
+    public void readAll() {
+
+    }
+
+    @GetMapping("/{boardId}")
+    public void read(@PathVariable Long boardId) {
+
+    }
+
+    @PatchMapping("/{boardId}")
+    public void update(@PathVariable Long boardId) {
+
+    }
+
+    @DeleteMapping("/{boardId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long boardId) {
+
+    }
 }

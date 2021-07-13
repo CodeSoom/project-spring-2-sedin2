98a738dd7120a7b803ed7035a979aff968a1d6c2
sedin <sedin@kakao.com>
2021-07-13 22:27:55 +0900

    Feat: BoardApiRequest DTO 작성
    - 사용자가 게시글에 관한 http 요청시 데이터를 받을 DTO 추가

---
+++ app/src/main/java/board/network/request/BoardApiRequest.java
@@ -0,0 +1,19 @@
+package board.network.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardApiRequest {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+}
